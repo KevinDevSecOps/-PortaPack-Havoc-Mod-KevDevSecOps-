4048087e022313ba770c59c26e7f4de1dde5d569
CK <kevinpamiestrabajo@gmail.com>
2025-07-01 18:21:30 +0200

    Update replay_attack.hpp

--- Firmware/Application/replay_attack/replay_attack.hpp
+++ Firmware/Application/replay_attack/replay_attack.hpp
@@ -1,3 +1,20 @@
+#include "replay_attack.hpp"
+#include "io_file.hpp"
+
+namespace ui {
+
+ReplayAttackView::ReplayAttackView(NavigationView& nav) {
+    add_children({ /* Botones/UI aquí */ });
+}
+
+void ReplayAttackView::on_save(const std::string& filename) {
+    File file;
+    if (file.create(filename)) {  // Usa la API de archivos del PortaPack
+        file.write(signal_data);  // 'signal_data' debe contener las muestras IQ
+    }
+}
+
+} // namespace ui
 #pragma once
 #include "ui.hpp"
 #include "transmitter_model.hpp"
