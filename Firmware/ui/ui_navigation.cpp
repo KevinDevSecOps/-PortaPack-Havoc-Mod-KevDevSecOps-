abc4812f3af0dffe79154c7ff9b2ccab5896ff17
CK <kevinpamiestrabajo@gmail.com>
2025-07-01 18:22:17 +0200

    Update ui_navigation.cpp

--- Firmware/ui/ui_navigation.cpp
+++ Firmware/ui/ui_navigation.cpp
@@ -1,3 +1,7 @@
+// Añade esto en el vector 'app_settings':
+{ "Jammer Mod", &ui::Bitmap::jammer_icon, [](NavigationView& nav) { nav.push<JammerModView>(); } },
+{ "RF Scanner", &ui::Bitmap::scan_icon, [](NavigationView& nav) { nav.push<ScannerModView>(); } },
+{ "Replay Tool", &ui::Bitmap::replay_icon, [](NavigationView& nav) { nav.push<ReplayAttackView>(); } },
 // Añade esto al vector 'app_settings':
 { "Jammer Mod", &ui::Bitmap::jammer_icon, [](NavigationView& nav) { nav.push<JammerModView>(); } },
 { "Scanner Mod", &ui::Bitmap::scan_icon, [](NavigationView& nav) { nav.push<ScannerModView>(); } },
