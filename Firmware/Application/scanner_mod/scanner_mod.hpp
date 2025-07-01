b8fe59cc58c49120b69741041a5397a4e8326357
CK <kevinpamiestrabajo@gmail.com>
2025-07-01 18:21:07 +0200

    Update scanner_mod.hpp

--- Firmware/Application/scanner_mod/scanner_mod.hpp
+++ Firmware/Application/scanner_mod/scanner_mod.hpp
@@ -1,3 +1,28 @@
+#include "scanner_mod.hpp"
+#include "spectrum_collector.hpp"
+
+namespace ui {
+
+ScannerModView::ScannerModView(NavigationView& nav) {
+    add_children({ /* Añade aquí tus componentes UI */ });
+
+    receiver.set_sampling_rate(4000000);  // 4 Msps
+    receiver.set_baseband_bandwidth(3500000);
+}
+
+void ScannerModView::start_scan() {
+    receiver.set_frequency(frequency);  // Define 'frequency' como variable de clase
+    SpectrumCollector::start();  // Usa el recolector de espectro del firmware base
+    is_scanning = true;
+}
+
+void ScannerModView::stop_scan() {
+    SpectrumCollector::stop();
+    receiver.disable();
+    is_scanning = false;
+}
+
+} // namespace ui
 #pragma once
 #include "ui.hpp"
 #include "receiver_model.hpp"
